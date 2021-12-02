86c767fb882269b04a0b52fcbeb50b53e93da601
deep846 <sanandachoudhury69@gmail.com>
2021-12-02 20:32:13 +0530

    Created item models and failure massege data model and fetch all data using post methode form notion API

--- lib/item_model.dart
+++ lib/item_model.dart
@@ -4,4 +4,15 @@ class Item{
   final double price;
   final DateTime date;
   const Item({required this.name , required this.category, required this.price, required this.date});
+
+  factory Item.fromMap(Map<String, dynamic> map) {    //take data as a Map as map
+    final properties = map['properties'] as Map<String, dynamic>;
+    final dateStr = properties['Date']?['date']?['start'];
+    return Item(
+        name: properties['Name']?['title']?[0]?['plain_text'] ?? '?',
+        category: properties['category']?['select']?['name'] ?? 'Any',
+      price: (properties['price']?['Number']?['name'] ?? 0).toDouble(),
+      date: dateStr != null ? DateTime.parse(dateStr) : DateTime.now(),
+    );
+  }
 }
