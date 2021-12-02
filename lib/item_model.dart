30ec7e1f92f832b289df7d5b0f8a5a1d9c8ed972
deep846 <sanandachoudhury69@gmail.com>
2021-12-02 19:11:08 +0530

    creating Item Model for our Budget app

---
+++ lib/item_model.dart
@@ -0,0 +1,7 @@
+class Item{
+  final String name;
+  final String category;
+  final double price;
+  final DateTime date;
+  const Item({required this.name , required this.category, required this.price, required this.date});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
-      home: Scaffold(),
+      home: BudgetScreen(),
     );
   }
 }
+
+
+
+ class BudgetScreen extends StatefulWidget {
+   const BudgetScreen({Key? key}) : super(key: key);
+
+   @override
+   _BudgetScreenState createState() => _BudgetScreenState();
+ }
+
+ class _BudgetScreenState extends State<BudgetScreen> {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold();
+   }
+ }
+
